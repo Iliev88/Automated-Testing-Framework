e8c4683611f83731c20e6bb7f0dad6dd3997cab4
Iliya Iliev <iliev.ilya@yahoo.com>
2018-03-24 12:14:45 +0200

    adding two more smoke tests

---
+++ WordpressAutomation/WordpressAutomation/NewPostPage.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WordpressAutomation
+{
+    public class NewPostPage
+    {
+        public static object Title
+        {
+            get
+            {
+                var title = Driver.Instance.FindElement(By.Id("title"));
+
+                if (title != null)
+                {
+                    return title.GetAttribute("value");
+                }
+                else
+                {
+                    return String.Empty;
+                }
+            }
+        }
+
+        public static void GoTo()
+        {
+            var menuPosts = Driver.Instance.FindElement(By.Id("menu-posts"));
+            menuPosts.Click();
+
+            var addNew = Driver.Instance.FindElement(By.LinkText("Add New"));
+            addNew.Click();
+        }
+
+        public static CreatePostCommand CreatePost(string title)
+        {
+            return new CreatePostCommand(title);
+        }
+
+        public static void GoToNewPost()
+        {
+            var message = Driver.Instance.FindElement(By.Id("message"));
+            var newPostLink = message.FindElements(By.TagName("a"))[0];
+            newPostLink.Click();
+        }
+
+        public static bool IsInEditMode()
+        {
+            return Driver.Instance.FindElement(By.XPath("//*[@id=\"wpbody-content\"]/div[3]/h1")) != null;
+        }
+    }
+
+    public class CreatePostCommand
+    {
+        private readonly string title;
+        private string body;
+
+        public void Publish()
+        {
+            Driver.Instance.FindElement(By.Id("title")).SendKeys(title);
+
+            Driver.Instance.SwitchTo().Frame("content_ifr");
+            Driver.Instance.SwitchTo().ActiveElement().SendKeys(body);
+            Driver.Instance.SwitchTo().DefaultContent();
+
+            Thread.Sleep(1000);
+
+            Driver.Instance.FindElement(By.Id("publish")).Click();
+        }
+
+        public CreatePostCommand WithBody(string body)
+        {
+            this.body = body;
+            return this;
+        }
+
+        public CreatePostCommand(string title)
+        {
+            this.title = title;
+        }
+    }
+}
